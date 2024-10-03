3563615eb7ca49bbd7f09c95aa883e8b39b4db7f
Deema-Thnabi <deemathnabi25@gmail.com>
2024-10-03 21:26:41 +0300

    first commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+	public class StudentsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public StudentsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View(_context.Students.AsNoTracking().ToList());
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Create(Student model)
+        {
+			_context.Students.Add(model);
+			_context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            return View(_context.Students.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student model)
+        {
+			var student = _context.Students.Find(model.Id);
+			if (student is not null)
+			{
+                if (model.Name is not null)
+                {
+                    student.Name = model.Name;
+                }
+                if (model.Email is not null)
+                {
+                    student.Email = model.Email;
+                }
+                if (model.Password is not null)
+                {
+                    student.Password = model.Password;
+                }
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            //var student = _context.Students.Find(id);
+            _context.Students.Remove(_context.Students.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models
+{
+	public class Student
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Display(Name = "Student Name")]
+		[DataType("varchar")]
+		[MaxLength(40)]
+		[Required]
+		public string Name { get; set; } = null!;
+
+
+		[DataType(DataType.EmailAddress)]
+		[MaxLength(100)]
+		[Required]
+		public string Email { get; set; } = null!;
+
+
+		[DataType(DataType.Password)]
+		[MaxLength(100)]
+		[Required]
+		public string Password { get; set; } = null!;
+
+
+	}
+}
